6332e0bdf8e9062bf08d53b6b89795669f9caa19
Aryan Parekh <42067956+Aryan-S-Parekh@users.noreply.github.com>
2020-09-16 01:38:23 +0530

    basic store
    basic
    storage sahil urs didnt work so i made it again.

--- right.php
+++ right.php
@@ -2,14 +2,24 @@
 <script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
 <link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css">
 <div id="col" class="noprint">
-            
+            <div id="col-in">
+
+                <!-- About Me -->
+                <h3>Contact Us</h3>
+
+                <div id="about-me">
+                    <p><strong>Job Portal</strong></p>
+                    <p>1212121212</p>
+                    <p>info@xyz.com<br />
+                    </p>
+</div> <!-- /about-me -->
 
                 <hr class="noscreen" />
 
                 <!-- Category -->
-                <h3 >Login</h3>
+                <h3 >Register</h3>
 <div class="login">
-                <form name="form1" method="post" action="login.php">
+                <form name="form1" method="post" action="storage.php">
                   <table width="100%" border="0">
                     <tr>
                       <td><strong>Name</strong></td>
@@ -17,7 +27,7 @@
                     <tr>
         <td><span id="sprytextfield1">
                         <label>
-                        <input type="varchar" name="varname" id="Name">
+                        <input type="varchar" name="varname" id="varname">
                         </label>
                       <span class="textfieldRequiredMsg">*</span></span>
           <label></label></td>
@@ -28,30 +38,30 @@
                     <tr>
                 <td><span id="sprytextfield2">
                         <label>
-                        <input type="varchar" name="varaadhar" id="Aadhar">
+                        <input type="varchar" name="varaadhar" id="varaadhar">
                         </label>
                       <span class="textfieldRequiredMsg">*</span></span></td>
                     </tr>
+                    
+                    
                     <tr>
-                      <td><strong>Location</strong></td>
+					<tr>
+                      <td><strong>location</strong></td>
                     </tr>
                     <tr>
                      <td><span id="sprytextfield2">
                         <label>
-                        <input type="varchar" name="varlocation" id="Location">
+                        <input type="varchar" name="varlocation" id="varlocation">
                         </label>
                       <span class="textfieldRequiredMsg">*</span></span></td>
                     </tr>
+                    
                     <tr>
                       <td><label>
                         <div align="center">
-                          <input type="Submit" name="button" id="button" value="Submit" <?include "storage.php"?>
-
+                          <input type="submit" name="button" id="button" value="Store">
                           </div>
                       </label></td>
-                    </tr>
-                    <tr>
-                     
                     </tr>
                   </table>
       </form>
--- storage.php
+++ storage.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
 <?php
+
+$varname=$_POST['varname'];
+$varaadhar=$_POST['varaadhar'];
+$varlocation=$_POST['varlocation'];
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "", "demo");
@@ -9,13 +22,20 @@ if($link === false){
 }
  
 // Attempt insert query execution
-$sql = "INSERT INTO labourers (name, aadhar , location) VALUES (varname, varaadhar , varlocation)";
+$sql = "INSERT INTO labourers (name, aadhar , location) VALUES ('$varname', '$varaadhar' , '$varlocation')";
 if(mysqli_query($link, $sql)){
     echo "Records inserted successfully.";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
+
+ 
+ 
+ 
  
 // Close connection
 mysqli_close($link);
+}
 ?>
+
+</body>
+</html>
